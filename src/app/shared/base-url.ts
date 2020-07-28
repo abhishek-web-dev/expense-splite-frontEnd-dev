598bd1a3ed04dfba56df3029f9f8834fa8d0e10d
Abhishek <51155348+abhishek-web-dev@users.noreply.github.com>
2020-07-28 07:11:21 +0530

    Update base-url.ts
    version 1.0.0

--- src/app/shared/base-url.ts
+++ src/app/shared/base-url.ts
@@ -1,8 +1,8 @@
 export class BaseURL {
     // For Local
-     //public static BASE_USER_ENDPOINT = "http://localhost:3000/api/v1/users/";
+     public static BASE_USER_ENDPOINT = "http://localhost:3000/api/v1/users/";
   
     // For Production
-    public static BASE_USER_ENDPOINT = "http://api.expense.abhiweb.xyz/api/v1/users/";
+    //public static BASE_USER_ENDPOINT = "your server url";
   }
   
